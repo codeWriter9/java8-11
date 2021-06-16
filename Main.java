4de442097b3c190d9470b61fecb0f5b2d482a8e6
codeWriter9 <nerdsubhankar2006@gmail.com>
2021-06-16 13:33:45 +0000

    Runnable in old way and new way

--- Main.java
+++ Main.java
@@ -1,29 +1,28 @@
-import java.util.Date;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-
-
 class Main {    
 
-
   public static void main(String[] args) {    
-    System.out.println("Java 8 Date Time API");
-    LocalDate today = LocalDate.now();    
-    LocalDateTime startOfToday = today.atStartOfDay();
-    LocalDateTime now = LocalDateTime.now();
-    System.out.println(" start of today:=" + startOfToday);
-    System.out.println(" Current Time  :=" + now);
-    long hours = startOfToday.until( now, ChronoUnit.HOURS ) % 24;
-    long mins  = startOfToday.until( now, ChronoUnit.MINUTES ) % 60;
-    long secs  = startOfToday.until( now, ChronoUnit.SECONDS );
-    System.out.println(hours + " : " + mins + " : " + secs);
-    ZonedDateTime zonedDateTime = today.atStartOfDay(ZoneId.systemDefault());
+    // Old way
+    new RunnableDemo().oldWay().start();    
+   // New way
+   new RunnableDemo().newWay().start();
+  }
+}
+
+class RunnableDemo {
+
+  public Thread oldWay() {
+    // Old way
+    return new Thread(new Runnable()
+      {
+        public void run() {
+          System.out.println("Inner class");
+        }
+      }
+    );
   }
 
+  public Thread newWay() {
+    // New way
+   return new Thread(() -> System.out.println("Using Lambda"));
+  }
 }
