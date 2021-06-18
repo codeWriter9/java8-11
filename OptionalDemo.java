49aacd1b86a24a7c707c3b9c31e5a87648a8f034
codeWriter9 <nerdsubhankar2006@gmail.com>
2021-06-18 05:30:46 +0000

    Add - optional demo

---
+++ OptionalDemo.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.function.Consumer;
+
+public class OptionalDemo {
+  public List<String> mcu() {
+    return Arrays.asList("Iron Man", "The Incredible Hulk",
+    "Iron Man 2", "Thor", "Captain America: The First Avenger",
+    "Marvel's The Avengers", "Iron Man 3", "Thor: The Dark World",
+    "Captain America: The Winter Soldier", "Guardians of the Galaxy",
+    "Avengers: Age of Ultron", "Ant-Man", "Captain America: Civil War", "Doctor Strange", "Guardians of the Galaxy Vol. 2",
+    "Spider-Man: Homecoming", "Thor: Ragnarok", "Black Panther",
+    "Avengers: Infinity War", "Ant-Man and the Wasp", "Captain Marvel", "Avengers: Endgame", "Spider-Man: Far From Home");
+  }
+}
